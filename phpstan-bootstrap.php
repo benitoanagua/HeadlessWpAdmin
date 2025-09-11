034ca4b4109e2388d27025bb1a51e43ed424ccd8
Benito Anagua <benito.anagua@gmail.com>
2025-09-11 15:50:30 -0400

    PublicInterface fixed

--- phpstan-bootstrap.php
+++ phpstan-bootstrap.php
@@ -6,18 +6,127 @@ if (!defined('ABSPATH')) {
     define('ABSPATH', '/tmp/wordpress/');
 }
 
-if (!defined('WP_DEBUG')) {
-    define('WP_DEBUG', true);
+// Definir constantes comunes de WordPress
+$wp_constants = [
+    'WP_DEBUG',
+    'WP_CONTENT_DIR',
+    'WP_CONTENT_URL',
+    'WP_PLUGIN_DIR',
+    'WP_PLUGIN_URL',
+    'WPLANG',
+    'WPINC',
+    'TEMPLATEPATH',
+    'STYLESHEETPATH',
+    'DB_HOST',
+    'DB_NAME',
+    'DB_USER',
+    'DB_PASSWORD',
+    'DB_CHARSET',
+    'DB_COLLATE',
+    'AUTH_KEY',
+    'SECURE_AUTH_KEY',
+    'LOGGED_IN_KEY',
+    'NONCE_KEY',
+    'AUTH_SALT',
+    'SECURE_AUTH_SALT',
+    'LOGGED_IN_SALT',
+    'NONCE_SALT'
+];
+
+foreach ($wp_constants as $constant) {
+    if (!defined($constant)) {
+        define($constant, '');
+    }
 }
 
+// Definir algunas funciones básicas de WordPress si no existen
 if (!function_exists('plugin_dir_path')) {
-    function plugin_dir_path($file) { return dirname($file) . '/'; }
+    function plugin_dir_path($file)
+    {
+        return dirname($file) . '/';
+    }
 }
 
 if (!function_exists('plugin_dir_url')) {
-    function plugin_dir_url($file) { return 'http://example.com/wp-content/plugins/' . basename(dirname($file)) . '/'; }
+    function plugin_dir_url($file)
+    {
+        return 'http://example.com/wp-content/plugins/' . basename(dirname($file)) . '/';
+    }
 }
 
 if (!function_exists('plugin_basename')) {
-    function plugin_basename($file) { return basename(dirname($file)) . '/' . basename($file); }
+    function plugin_basename($file)
+    {
+        return basename(dirname($file)) . '/' . basename($file);
+    }
+}
+
+if (!function_exists('load_plugin_textdomain')) {
+    function load_plugin_textdomain($domain, $deprecated = false, $rel_path = false)
+    {
+        return true;
+    }
+}
+
+if (!function_exists('add_action')) {
+    function add_action($tag, $function_to_add, $priority = 10, $accepted_args = 1)
+    {
+        return true;
+    }
+}
+
+if (!function_exists('add_filter')) {
+    function add_filter($tag, $function_to_add, $priority = 10, $accepted_args = 1)
+    {
+        return true;
+    }
+}
+
+if (!function_exists('is_admin')) {
+    function is_admin()
+    {
+        return false;
+    }
+}
+
+if (!function_exists('wp_enqueue_script')) {
+    function wp_enqueue_script($handle, $src = '', $deps = [], $ver = false, $in_footer = false)
+    {
+        return true;
+    }
+}
+
+if (!function_exists('wp_enqueue_style')) {
+    function wp_enqueue_style($handle, $src = '', $deps = [], $ver = false, $media = 'all')
+    {
+        return true;
+    }
+}
+
+if (!function_exists('flush_rewrite_rules')) {
+    function flush_rewrite_rules($hard = true)
+    {
+        return true;
+    }
+}
+
+if (!function_exists('register_activation_hook')) {
+    function register_activation_hook($file, $function)
+    {
+        return true;
+    }
+}
+
+if (!function_exists('register_deactivation_hook')) {
+    function register_deactivation_hook($file, $function)
+    {
+        return true;
+    }
+}
+
+if (!function_exists('register_uninstall_hook')) {
+    function register_uninstall_hook($file, $function)
+    {
+        return true;
+    }
 }
--- src/Frontend/PublicInterface.php
+++ src/Frontend/PublicInterface.php
@@ -5,22 +5,29 @@ namespace HeadlessWPAdmin\Frontend;
 /**
  * Clase para manejar la interfaz pública
  */
-class PublicInterface {
-    
-    public function __construct() {
+class PublicInterface
+{
+
+    public function __construct()
+    {
         add_action('wp_footer', [$this, 'renderFrontendApp']);
         add_shortcode('headless-wp-admin', [$this, 'renderShortcode']);
     }
-    
-    public function renderFrontendApp(): void {
+
+    public function renderFrontendApp(): void
+    {
         // Renderizar app frontend si es necesario
     }
-    
-    public function renderShortcode($atts): string {
+
+    /**
+     * @param array<string, mixed> $atts Atributos del shortcode
+     */
+    public function renderShortcode(array $atts = []): string
+    {
         $atts = shortcode_atts([
             'type' => 'default'
         ], $atts);
-        
+
         return '<div id="headless-wp-admin-shortcode" data-type="' . esc_attr($atts['type']) . '"></div>';
     }
 }
